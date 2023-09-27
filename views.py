da43e8ce79bd5169a46e01da12ad1c7c9e0128f5
Elvonzinio <barlowskiwojciech@gmail.com>
2023-09-27 12:37:06 +0200

    gauss

--- views.py
+++ views.py
@@ -5,3 +5,8 @@ views = Blueprint(__name__, 'views')
 @views.route('/')
 def index():
     return render_template('index.html')
+
+
+@views.route('/login')
+def login():
+    return render_template('login.html')
